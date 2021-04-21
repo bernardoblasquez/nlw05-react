af88e105472901d4c95fc48208c2d6fe30e63103
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-21 09:56:58 -0300

    [aula 02] [feature]: utilizaçãodo pacote date-fns para imprimir data atual na header

--- podcastrnext/src/components/Header/index.tsx
+++ podcastrnext/src/components/Header/index.tsx
@@ -1,14 +1,16 @@
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
 import styles from './styles.module.scss'
 
 const Header = () => {
 
-    
+    const currentDate = format(new Date, 'EEEEEE, dd, MMMM', {locale: ptBR});
 
     return(
         <header className={styles.headerContainer}>
             <img src="/logo.svg" alt="Podcaster"/>
             <p>O melhor para vc ouvir, sempre</p>
-            <span>qui, 8 abril</span>
+            <span>{currentDate}</span>
         </header>        
     );
 }
