3a373fbed4eb0922d28e7fcfc50cf325fd31e633
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-22 08:22:23 -0300

    [aula 2] [feature]: adição de conteúdo na página usando os conceitos de SSG. Nos comentários tem a implementação com SPA e SSR

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -1,7 +1,80 @@
-import Header from '../components/Header/index'
+// SSR
+// SSG
 
-export default function Home() {
+
+
+export default function Home(props) {
+  
+
   return (
-    <Header />
-  )
+    <div>
+      <h1>Index</h1>
+      <div>{JSON.stringify(props.episodes)}</div>
+    </div>
+  ) 
 }
+
+export async function getStaticProps(){
+  
+      const response = await fetch('http://localhost:3333/episodes');
+      const data = await response.json();
+    
+      return {
+        props: {
+          episodes: data 
+        },
+        revalidate: 60*60*8,
+      } 
+}
+
+/*
+  SPA --------------------------
+    useEffect("What I wanna execute [function]" , "When i wanna execute")
+    useEffect( ()=> {}, var)
+    useEffect( ()=> {}, '') execute only once
+  
+    import {useEffect} from 'react'
+    Use inside Home()
+
+    useEffect(() =>{
+      fetch('http://localhost:3333/episodes')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }) // SPA way -> it is not indexable by browsers
+  
+
+    SSR --------------------------
+    - It is executed every time a user access the page.
+
+    export async function getServerSideProps(){
+  
+      const response = await fetch('http://localhost:3333/episodes');
+      const data = await response.json();
+      
+      console.log("---------------")
+      console.log(data)
+      console.log("-------------------")
+    
+      return {
+        props: {
+          episodes: data 
+        } 
+      } 
+    }
+
+
+    SSG --------------------------
+    export async function getStaticProps(){
+  
+      const response = await fetch('http://localhost:3333/episodes');
+      const data = await response.json();
+    
+      return {
+        props: {
+          episodes: data 
+        },
+        revalidate: 60*60*8,
+      } 
+    }
+
+*/
