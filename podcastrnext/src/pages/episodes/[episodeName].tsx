53f7f757e6be8a89b57edf2ccac839116a18e785
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-23 12:39:10 -0300

    [aula 03] [feature]: criação da estrutura html para receber os dados detalhados do epsódio.

--- podcastrnext/src/pages/episodes/[episodeName].tsx
+++ podcastrnext/src/pages/episodes/[episodeName].tsx
@@ -1,7 +1,10 @@
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
+import styles from './episode.module.scss'
 import { api } from '../../components/services/api';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
@@ -23,7 +26,39 @@ type EpisodeProps = {
 export default function Episodes ({ episode }: EpisodeProps) {
         
     return(
-        <h1>{episode.title}</h1>
+        <div className={styles.episode}>
+            <div className={styles.thumbnailContainer}>
+                <Link href="/">
+                    <button type="button">
+                        <img src="/arrow-left.svg" alt="Voltar"/>
+                    </button>
+                </Link>
+                
+                <Image 
+                    width={700} 
+                    height={160} 
+                    src={episode.thumbnail}  
+                    alt={episode.title}
+                    objectFit='cover'/>
+
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episódio"/>
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+            </header>
+
+            <div 
+                className={styles.description} 
+                dangerouslySetInnerHTML={{ __html:episode.description }}>
+            </div>
+
+        </div>
     )
 }
 
@@ -40,8 +75,6 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
     const { episodeName } = context.params; 
     const { data } = await api.get(`/episodes/${episodeName}`)
-
-   // console.log(episodeName)
 
     const episode =  {
         id: data.id,
