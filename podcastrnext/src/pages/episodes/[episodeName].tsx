6753f139bef8c4152af5d1e8d1dd34d1c2b32ae2
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-23 09:53:44 -0300

    [aula 03] [feature]: criação da função [getStaticProps] e configuração dos tipos usados na "pagina"

--- podcastrnext/src/pages/episodes/[episodeName].tsx
+++ podcastrnext/src/pages/episodes/[episodeName].tsx
@@ -1,16 +1,61 @@
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticProps } from 'next';
 import { useRouter } from 'next/router'
+import { api } from '../../components/services/api';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 
+type Episode = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    members: string;
+    duration: number;
+    durationAsString: string;
+    publishedAt: string;
+    description: string;
+  }
+  
+type EpisodeProps = {
+    episode: Episode
+}
 
-const episodes = () =>{
-    
-    const router = useRouter();
-    
+export default function Episodes ({ episode }: EpisodeProps) {
+        
     return(
-        <h1>{router.query.episodeName}</h1>
+        <h1>{episode.title}</h1>
     )
 }
 
-export default episodes;
+
+export const getStaticProps: GetStaticProps = async (context) => {
+
+    const { episodeName } = context.params; 
+    const { data } = await api.get(`/episodes/${episodeName}`)
+
+    console.log(episodeName)
+
+    const episode =  {
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail, 
+        members: data.members,
+        publishedAt: format(parseISO(data.published_at), 'd MM yy', {locale: ptBR} ),
+        duration: Number(data.file.duration),
+        durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+        description: data.description,
+    }
+
+    return{
+        props:{
+            episode,
+        },
+        revalidate: 60*60*24, //24 hours
+    }
+
+
+
+} 
 
 
 // episodeName - está entre [] no nome do arquivo
