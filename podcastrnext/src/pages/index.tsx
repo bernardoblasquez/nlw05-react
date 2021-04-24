8a643ef1d863f1256cf47914593d3c89a6d5b5ea
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-24 19:17:21 -0300

    [aula 04] [fix]: correção de código: atribuição  do id so episodio para o parametro key

--- podcastrnext/src/pages/index.tsx
+++ podcastrnext/src/pages/index.tsx
@@ -82,7 +82,7 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
               
               return(
 
-                <tr>
+                <tr key={episode.id}>
                   <td style={{width: 72}}>
                     <Image 
                       width={120} 
