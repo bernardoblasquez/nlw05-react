83dc868987108b98a8e50e376e32b939e2c3204e
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-25 10:26:57 -0300

    [aula 05] [refact]: refatoração de app e PlayerContext para melhorar legibilidade de código

--- podcastrnext/src/contexts/PlayerContext.tsx
+++ podcastrnext/src/contexts/PlayerContext.tsx
@@ -1,21 +1,59 @@
-import { createContext } from "react"
+import { createContext, useState, ReactNode } from "react";
 
 type Episode = {
-    title: string;
-    members: string;
-    thumbnail: string;
-    duration: number;
-    url: string;
-}
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
+};
 
 type PlayerContextData = {
-    episodeList: Episode[];
-    currentEpisodeIndex: number;
-    isPlaying: boolean;
-    play: (episode: Episode) => void;
-    setPlayingState: (state:boolean) => void;
-    togglePlay: () => void;
+  episodeList: Episode[];
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
+  play: (episode: Episode) => void;
+  setPlayingState: (state: boolean) => void;
+  togglePlay: () => void;
+};
+
+type PlayerContextProviderProps = {
+    children: ReactNode
 }
 
-
 export const PlayerContext = createContext({} as PlayerContextData); // defino o formato dos dados que vou salvar no contexto
+
+export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode: Episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        episodeList,
+        currentEpisodeIndex,
+        play,
+        isPlaying,
+        setPlayingState,
+        togglePlay,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+}
