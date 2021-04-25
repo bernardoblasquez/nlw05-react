40cabdef27b4aba4e8b43c9d7d01dcbf74173d3a
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-24 21:09:35 -0300

    [aula 4] [feature]: criação dos tipos e do formato de dados que PlayerContext vai receber

--- podcastrnext/src/contexts/PlayerContext.tsx
+++ podcastrnext/src/contexts/PlayerContext.tsx
@@ -1,3 +1,18 @@
 import { createContext } from "react"
 
-export const PlayerContext = createContext('diego'); // defino o formato dos dados que vou salvar no contexto
+type Episode = {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+}
+
+type PlayerContextData = {
+    episodeList: Episode[];
+    currentEpisodeIndex: number;
+    play: (episode: Episode) => void;
+}
+
+
+export const PlayerContext = createContext({} as PlayerContextData); // defino o formato dos dados que vou salvar no contexto
