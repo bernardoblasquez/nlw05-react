1fcc765d3e47daa0dc3a180bbe8ef1178fe6d1da
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-04-25 16:03:05 -0300

    [aula 05] [feature]: criação das funções playList, playNext e playPrevious  e atualização do estado na aplicação.

--- podcastrnext/src/contexts/PlayerContext.tsx
+++ podcastrnext/src/contexts/PlayerContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useContext } from "react";
 
 type Episode = {
   title: string;
@@ -13,17 +13,24 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   play: (episode: Episode) => void;
+  playList: (list: Episode[], index: number) => void;
   setPlayingState: (state: boolean) => void;
   togglePlay: () => void;
+  playNext: () => void;
+  playPrevious: () => void;
+  hasNext: boolean;
+  hasPrevious: boolean
 };
 
 type PlayerContextProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
-export const PlayerContext = createContext({} as PlayerContextData); // defino o formato dos dados que vou salvar no contexto
+export const PlayerContext = createContext({} as PlayerContextData);
+// defino o formato dos dados que vou salvar no contexto
 
 export function PlayerContextProvider({ children }: PlayerContextProviderProps) {
+
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
@@ -31,6 +38,12 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   function play(episode: Episode) {
     setEpisodeList([episode]);
     setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function playList(list: Episode[], index: number) {
+    setEpisodeList(list);
+    setCurrentEpisodeIndex(index);
     setIsPlaying(true);
   }
 
@@ -42,18 +55,42 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsPlaying(state);
   }
 
+  const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
+  const hasPrevious = currentEpisodeIndex > 0;
+  
+  function playNext() {
+    if (hasNext) {
+      setCurrentEpisodeIndex(currentEpisodeIndex + 1);
+    }
+  }
+
+  function playPrevious() {
+    if (hasPrevious) {
+      setCurrentEpisodeIndex(currentEpisodeIndex - 1);
+    }
+  }
+
   return (
     <PlayerContext.Provider
       value={{
         episodeList,
         currentEpisodeIndex,
         play,
+        playList,
+        playNext,
+        playPrevious,
         isPlaying,
-        setPlayingState,
         togglePlay,
+        setPlayingState,
+        hasNext,
+        hasPrevious,
       }}
     >
       {children}
     </PlayerContext.Provider>
   );
+}
+
+export const usePlayer = () => {
+  return useContext(PlayerContext)
 }
